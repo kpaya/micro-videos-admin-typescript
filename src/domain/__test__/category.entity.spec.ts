eec41a1cb9318e5b39846973d0f9581e6ed0847f
Kpaya <matteus.santos03@gmail.com>
2023-09-16 18:46:27 -0300

    test: add category entity tests

--- src/domain/__test__/category.entity.spec.ts
+++ src/domain/__test__/category.entity.spec.ts
@@ -41,4 +41,66 @@ describe("Category Unit Test", () => {
 			expect(category.isActive).toBe(true);
 		});
 	});
+
+	describe("methods", () => {
+		it("should update the category name", () => {
+			const category = new Category({
+				name: "Category 1",
+			});
+
+			expect(category.name).toBe("Category 1");
+
+			category.changeName("Novo nome");
+			expect(category.name).toBe("Novo nome");
+		});
+
+		it("should update the category description", () => {
+			const category = new Category({
+				name: "Category 1",
+			});
+
+			expect(category.description).toBeNull();
+
+			category.changeDescription("Description");
+			expect(category.description).toBe("Description");
+		});
+
+		it("should activate the category", () => {
+			const category = new Category({
+				name: "Category 1",
+				isActive: false,
+			});
+
+			category.activate();
+			expect(category.isActive).toBe(true);
+		});
+
+		it("should deactivate the category", () => {
+			const category = new Category({
+				name: "Category 1",
+				isActive: true,
+			});
+
+			category.deactivate();
+			expect(category.isActive).toBe(false);
+		});
+
+		it("should return json representation of the category", () => {
+			const createdAt = new Date();
+			const category = new Category({
+				name: "Category 1",
+				description: "Description",
+				isActive: false,
+				createdAt,
+			});
+
+			expect(category.toJSON()).toEqual({
+				categoryId: undefined,
+				name: "Category 1",
+				description: "Description",
+				isActive: false,
+				createdAt,
+			});
+		});
+	});
 });
