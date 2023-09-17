06edc9d1f30075035ad7a8de34988bc9ac66d53e
Kpaya <matteus.santos03@gmail.com>
2023-09-17 17:22:46 -0300

    refactor: fixing throw error in uuid value object

--- src/shared/value-objects/uuid.vo.ts
+++ src/shared/value-objects/uuid.vo.ts
@@ -2,23 +2,23 @@ import { ValueObject } from "../domain/value-object";
 import { v4 as uuidV4, validate as uuidValidate } from "uuid";
 
 export class Uuid extends ValueObject {
+	constructor(public readonly id?: string) {
+		super();
+		this.id = id || uuidV4();
+		this.validate(this.id);
+	}
 
-    constructor(public readonly id?: string) {
-        super();
-        this.id = id || uuidV4();
-        if (!this.isValid(this.id)){
-            throw new Error("Invalid UUID");
-        }
-    }
-
-    private isValid(id: string): boolean {
-        return uuidValidate(id);
-    }
+	private validate(id: string) {
+		const isValid = uuidValidate(id);
+		if (!isValid) {
+			throw new InvalidUuidError();
+		}
+	}
 }
 
 export class InvalidUuidError extends Error {
-    constructor(message?: string){
-        super(message || "Id must be a valid UUID");
-        this.name = "InvalidUuidError";
-    }
+	constructor(message?: string) {
+		super(message || "Id must be a valid UUID");
+		this.name = "InvalidUuidError";
+	}
 }
