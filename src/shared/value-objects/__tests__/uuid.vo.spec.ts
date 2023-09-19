c0a35462d70fa02c5f0399bbbedafd56a88bbfe4
Kpaya <matteus.santos03@gmail.com>
2023-09-19 20:49:02 -0300

    test: adding uuid factory method

--- src/shared/value-objects/__tests__/uuid.vo.spec.ts
+++ src/shared/value-objects/__tests__/uuid.vo.spec.ts
@@ -4,18 +4,18 @@ describe("UUID Value Object Unit Tests", () => {
 	const validateSpy = jest.spyOn(Uuid.prototype as any, "validate");
 
 	it("should throw an error if the UUID is invalid", () => {
-		expect(() => new Uuid("invalid-uuid")).toThrowError(
+		expect(() => Uuid.create("invalid-uuid")).toThrowError(
 			"Id must be a valid UUID"
 		);
 		expect(validateSpy).toHaveBeenCalledTimes(1);
 	});
 
 	it("should be a valid UUID", () => {
-		const uuid = new Uuid();
+		const uuid = Uuid.create();
 		expect(uuid).toBeInstanceOf(Uuid);
 		expect(uuid.id).toHaveLength(36);
 
-		const uuid2 = new Uuid("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11");
+		const uuid2 = Uuid.create("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11");
 		expect(uuid2).toBeInstanceOf(Uuid);
 		expect(uuid2.id).toBe("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11")
 
@@ -23,8 +23,8 @@ describe("UUID Value Object Unit Tests", () => {
 	});
 
 	it("should be the same UUID value object", () => {
-		const uuid1 = new Uuid("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11");
-		const uuid2 = new Uuid("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11");
+		const uuid1 = Uuid.create("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11");
+		const uuid2 = Uuid.create("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11");
 
 		expect(uuid1.equals(uuid2)).toBe(true);
 		expect(validateSpy).toHaveBeenCalledTimes(2);
